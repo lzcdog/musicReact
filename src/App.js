e4553d813bf682d32aae45698992fb4f5730e394
lzcdog <1046492069@qq.com>
2024-01-31 00:23:11 +0800

    1212

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Suspense, memo } from 'react'
 import { Provider } from 'react-redux'
 import { renderRoutes } from 'react-router-config'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import AppFooter from './components/app-footer'
 import AppHeader from './components/app-header'
 import AppPlayer from './views/app-player'
@@ -10,12 +10,12 @@ import store from './store'
 const App = memo(() => {
   return (
     <Provider store={store}>
-      <BrowserRouter>
+      <HashRouter>
         <AppHeader></AppHeader>
         <Suspense fallback={<div>loading</div>}>{renderRoutes(routes)}</Suspense>
         <AppFooter></AppFooter>
         <AppPlayer></AppPlayer>
-      </BrowserRouter>
+      </HashRouter>
     </Provider>
   )
 })
