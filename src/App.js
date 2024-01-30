a5efa2cefc662e1853f13d43a2f9a0971a711e71
lzcdog <1046492069@qq.com>
2024-01-31 00:11:56 +0800

    测试

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Suspense, memo } from 'react'
 import { Provider } from 'react-redux'
 import { renderRoutes } from 'react-router-config'
-import { HashRouter } from 'react-router-dom'
+import { HistoryRouter } from 'react-router-dom'
 import AppFooter from './components/app-footer'
 import AppHeader from './components/app-header'
 import AppPlayer from './views/app-player'
@@ -10,12 +10,12 @@ import store from './store'
 const App = memo(() => {
   return (
     <Provider store={store}>
-      <HashRouter>
+      <HistoryRouter>
         <AppHeader></AppHeader>
         <Suspense fallback={<div>loading</div>}>{renderRoutes(routes)}</Suspense>
         <AppFooter></AppFooter>
         <AppPlayer></AppPlayer>
-      </HashRouter>
+      </HistoryRouter>
     </Provider>
   )
 })
